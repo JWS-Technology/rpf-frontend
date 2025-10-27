d410d79b6cac1b3dd9af0ae8b5c3c94faf9cd5d3
Noel Sebu <noelsebu555@gmail.com>
2025-10-27 23:01:56 +0530

    [chore] added issue selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import IssueSelector from "./components/IssueSelector";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
    <div className="bg-white min-h-screen">
     <Navbar />
+    <IssueSelector />
    </div>
   );
 }
