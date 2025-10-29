11897a1ec83dffdf27b34d5dc324808ff653d3ae
Noel Sebu <noelsebu555@gmail.com>
2025-10-29 11:06:17 +0530

    [style] add space y to get gap

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-   <div className="bg-white min-h-screen">
+   <div className="bg-white min-h-screen space-y-14">
     <Navbar />
     <IssueSelector />
     <DialPad />
