71c5581bd9ab497afe0e90632d807322e13e2398
Noel Sebu <noelsebu555@gmail.com>
2025-11-01 20:40:13 +0530

    [format] prettier formatting

--- lib/features/sos-data/sosSlice.ts
+++ lib/features/sos-data/sosSlice.ts
@@ -1,42 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface sosData {
-  phone_number: string,
-  issue_type: string,
-  station: string,
+  phone_number: string;
+  issue_type: string;
+  station: string;
 }
 
 const initialState: sosData = {
   phone_number: "",
   issue_type: "",
   station: "",
-}
+};
 
 export const sosSlice = createSlice({
-  name: 'sos',
+  name: "sos",
   initialState,
   reducers: {
     setPhoneNumber: (state, action: PayloadAction<string>) => {
       state.phone_number = action.payload;
     },
     setStation: (state, action: PayloadAction<string>) => {
-      state.station =  action.payload;
+      state.station = action.payload;
     },
     setIssueType: (state, action: PayloadAction<string>) => {
       state.issue_type = action.payload;
     },
     clearPhoneNumber: (state) => {
-      state.phone_number = ""
+      state.phone_number = "";
     },
     clearAllData: (state) => {
       state.phone_number = "";
-      state.issue_type = "",
-    }
+      state.issue_type = "";
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { clearPhoneNumber, setPhoneNumber } = sosSlice.actions
+export const {
+  setPhoneNumber,
+  setStation,
+  setIssueType,
+  clearPhoneNumber,
+  clearAllData,
+} = sosSlice.actions;
 
-export default sosSlice.reducer
+export default sosSlice.reducer;
