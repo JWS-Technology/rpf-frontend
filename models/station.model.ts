39d0bc75ca60c557ba1bbe4897722e198191fd99
Noel Sebu <noelsebu555@gmail.com>
2025-11-03 10:29:56 +0530

    [feat] station model created

---
+++ models/station.model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const stationSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    code: { type: String },
+    location: { type: String },
+    contact_number: { type: String },
+    officer_in_charge: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.stations ||
+  mongoose.model("stations", stationSchema);
