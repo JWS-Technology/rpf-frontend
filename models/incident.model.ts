fe8e617eb82259f441f05422f352ad587d5e1d48
Noel Sebu <noelsebu555@gmail.com>
2025-11-03 10:26:40 +0530

    [feat] created incidents schema

---
+++ models/incident.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const incidentSchema = new mongoose.Schema(
+  {
+    issue_type: { type: String, required: true },
+    description: { type: String },
+    station: { type: String },
+    location: { type: String },
+    phone_number: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["Pending", "In-Progress", "Resolved"],
+      default: "Pending",
+    },
+    assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.incidents ||
+  mongoose.model("incidents", incidentSchema);
