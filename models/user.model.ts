576a9cb204bd870381f38b127a4ba86a7b4c49ba
Noel Sebu <noelsebu555@gmail.com>
2025-11-03 10:28:34 +0530

    [feat] user schema created

---
+++ models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    phone_number: { type: String },
+    role: {
+      type: String,
+      enum: ["Admin", "Officer", "Dispatcher"],
+      default: "Officer",
+    },
+    station: { type: String },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.users || mongoose.model("users", userSchema);
