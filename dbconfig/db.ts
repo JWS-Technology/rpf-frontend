67b082a96daaef5cc546e4858f6ca46c3189b59a
Noel Sebu <noelsebu555@gmail.com>
2025-11-08 21:17:14 +0530

    [fix] db uri bug fixed

--- dbconfig/db.ts
+++ dbconfig/db.ts
@@ -10,14 +10,13 @@ export async function connect(): Promise<void> {
   if (connectionStates.isConnected) {
     return;
   }
-
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI as string);
+    const db = await mongoose.connect(process.env.DB_URI as string);
 
     connectionStates.isConnected = db.connection.readyState;
     // console.log("✅ Database connected:", db.connection.host);
   } catch (error) {
     console.error("❌ Error in database connection\n", error);
-    process.exit(1);
+    // process.exit(1);
   }
 }
