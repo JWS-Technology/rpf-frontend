310041ac786b592eaa5b21b1f5f595d4cc1d31d7
Noel Sebu <noelsebu555@gmail.com>
2025-11-08 21:22:06 +0530

    [update] disabled few field in db

--- models/incident.model.ts
+++ models/incident.model.ts
@@ -3,24 +3,26 @@ import mongoose from "mongoose";
 const incidentSchema = new mongoose.Schema(
   {
     issue_type: { type: String, required: true },
-    description: { type: String },
+    // description: { type: String },
+    phone_number: { type: String, default: "nill" },
     station: { type: String },
-    location: { type: String },
-    phone_number: { type: String, required: true },
-    status: {
-      type: String,
-      enum: ["Pending", "In-Progress", "Resolved"],
-      default: "Pending",
-    },
-    assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    priority: {
-      type: String,
-      enum: ["Low", "Medium", "High"],
-      default: "Medium",
-    },
+    // location: { type: String },
+    // status: {
+    //   type: String,
+    //   enum: ["Pending", "In-Progress", "Resolved"],
+    //   default: "Pending",
+    // },
+    // assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    // priority: {
+    //   type: String,
+    //   enum: ["Low", "Medium", "High"],
+    //   default: "Medium",
+    // },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Incident ||
-  mongoose.model("Incident", incidentSchema);
+const Incident =
+  mongoose.models.Incident || mongoose.model("Incident", incidentSchema);
+
+export default Incident;
