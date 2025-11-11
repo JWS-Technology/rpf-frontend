fe63ee667a65e8e5914a07703159ac8ff1788e97
Joerakesh <rakeshjoe52@gmail.com>
2025-11-12 02:51:24 +0530

    document build error

--- lib/exportIncidents.ts
+++ lib/exportIncidents.ts
@@ -111,7 +111,7 @@ export async function exportIncidentsAsPDF(
   });
 
   // === Footer ===
-  const pageCount = doc.internal.getNumberOfPages();
+  const pageCount = doc.getNumberOfPages();
   for (let i = 1; i <= pageCount; i++) {
     doc.setPage(i);
     doc.setFontSize(9);
