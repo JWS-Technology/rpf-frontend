069020cde9b428f1adf954bfa871774b1bf0eaa6
Noel Sebu <noelsebu555@gmail.com>
2025-11-11 09:38:12 +0530

    [chore] commented out unused fields

--- models/incident.model.ts
+++ models/incident.model.ts
@@ -3,22 +3,9 @@ import mongoose from "mongoose";
 const incidentSchema = new mongoose.Schema(
   {
     issue_type: { type: String, required: true },
-    // description: { type: String },
     phone_number: { type: String, default: "nill" },
     station: { type: String },
     audio_url: {type: String},
-    // location: { type: String },
-    // status: {
-    //   type: String,
-    //   enum: ["Pending", "In-Progress", "Resolved"],
-    //   default: "Pending",
-    // },
-    // assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    // priority: {
-    //   type: String,
-    //   enum: ["Low", "Medium", "High"],
-    //   default: "Medium",
-    // },
   },
   { timestamps: true }
 );
@@ -27,3 +14,32 @@ const Incident =
   mongoose.models.Incident || mongoose.model("Incident", incidentSchema);
 
 export default Incident;
+// import mongoose from "mongoose";
+
+// const incidentSchema = new mongoose.Schema(
+//   {
+//     issue_type: { type: String, required: true },
+//     // description: { type: String },
+//     phone_number: { type: String, default: "nill" },
+//     station: { type: String },
+//     audio_url: {type: String},
+//     // location: { type: String },
+//     // status: {
+//     //   type: String,
+//     //   enum: ["Pending", "In-Progress", "Resolved"],
+//     //   default: "Pending",
+//     // },
+//     // assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//     // priority: {
+//     //   type: String,
+//     //   enum: ["Low", "Medium", "High"],
+//     //   default: "Medium",
+//     // },
+//   },
+//   { timestamps: true }
+// );
+
+// const Incident =
+//   mongoose.models.Incident || mongoose.model("Incident", incidentSchema);
+
+// export default Incident;
