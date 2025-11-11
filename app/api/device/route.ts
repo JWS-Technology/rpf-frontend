a3eb0217a64e6831e4c4ea1d283d747b20caf2bb
Noel Sebu <noelsebu555@gmail.com>
2025-11-11 12:21:37 +0530

    [feat] save device token to db

---
+++ app/api/device/route.ts
@@ -0,0 +1,24 @@
+import { connect } from "@/dbconfig/db";
+import Device from "@/models/device.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connect();
+    const formData = await req.formData();
+    const device_token = formData.get("device_token");
+    const newDeviceToken = new Device({device_token});
+    console.log(newDeviceToken);
+    return NextResponse.json(
+      { message: "message successfully sent", success: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    
+    return NextResponse.json(
+      { message: "failed in device", success: false },
+      { status: 200 }
+    );
+  }
+}
