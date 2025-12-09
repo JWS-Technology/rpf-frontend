38dc60fa635ae5fa45f66e6058d20f308cf4d589
Noel Sebu <noelsebu555@gmail.com>
2025-12-09 11:26:29 +0530

    new filed added to model

--- models/incident.model.ts
+++ models/incident.model.ts
@@ -11,6 +11,8 @@ const incidentSchema = new mongoose.Schema(
       default: "OPEN",
     },
     audio_url: { type: String },
+    officer: { type: String },
+    action_time: { type: String },
   },
   { timestamps: true }
 );
@@ -19,32 +21,3 @@ const Incident =
   mongoose.models.Incident || mongoose.model("Incident", incidentSchema);
 
 export default Incident;
-// import mongoose from "mongoose";
-
-// const incidentSchema = new mongoose.Schema(
-//   {
-//     issue_type: { type: String, required: true },
-//     // description: { type: String },
-//     phone_number: { type: String, default: "nill" },
-//     station: { type: String },
-//     audio_url: {type: String},
-//     // location: { type: String },
-//     // status: {
-//     //   type: String,
-//     //   enum: ["Pending", "In-Progress", "Resolved"],
-//     //   default: "Pending",
-//     // },
-//     // assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-//     // priority: {
-//     //   type: String,
-//     //   enum: ["Low", "Medium", "High"],
-//     //   default: "Medium",
-//     // },
-//   },
-//   { timestamps: true }
-// );
-
-// const Incident =
-//   mongoose.models.Incident || mongoose.model("Incident", incidentSchema);
-
-// export default Incident;
